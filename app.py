d95f4cabe63c241bff6a85976cf8adbbd44cd4b6
Kathleen0113 <61340799+Kathleen0113@users.noreply.github.com>
2022-06-08 00:47:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -88,11 +88,554 @@ def handle_postback(event):
         line_bot_api.reply_message(event.reply_token,TextSendMessage('請輸入捷運站名'))
         result = event.postback.data[2:].split('&')
 
-'''	
-# 蕭
+	
+# 蕭-爬googlemap,選出15個選項
+import googlemaps
+import pandas as pd
+import time
+google_key = 'AIzaSyB74lRYR0Y-8FGxnFAoW0V8Wv6GEcKmbr8' # fill in your google map api key
+gmaps = googlemaps.Client(key=google_key)
 
-# 妤
+# In[61]:
 
+from io import BytesIO
+from PIL import Image
+def time_transform(row):
+    if pd.isna(row):
+        return {}
+    return row['weekday_text']
+
+def image_transform(row):
+    b = row[0]['photo_reference']
+    photo = gmaps.places_photo(photo_reference = b, max_width=800, max_height=800)
+    photo
+    f = BytesIO()
+    for chunk in gmaps.places_photo(photo_reference = b, max_width=800, max_height=800):
+        if chunk:
+            f.write(chunk)
+    image = Image.open(f)
+    return image
+
+
+# In[62]:
+
+
+def find_sex_place(query: str, query_type: str, place_type: str):
+    if query_type == 'station':
+        ids = []
+        results = []
+        # Geocoding an address
+        geocode_result = gmaps.geocode(query)
+        loc = geocode_result[0]['geometry']['location']
+        query_result = gmaps.places_nearby(type='bar',location=loc, radius=1000)
+        results.extend(query_result['results'])
+        while query_result.get('next_page_token'):
+            time.sleep(2)
+            query_result = gmaps.places_nearby(page_token=query_result['next_page_token'])
+            results.extend(query_result['results'])
+        #print(f'Number of bars in {query}: {len(results)} (maximum: 60)')
+        for place in results:
+            ids.append(place['place_id'])
+        stores_info = []
+        ids = list(set(ids)) 
+        for id in ids:
+            stores_info.append(gmaps.place(place_id=id, language='zh-TW')['result'])
+        #print(f'Total number of bars: {len(stores_info)}')
+        taipei_bar = pd.DataFrame.from_dict(stores_info)
+        taipei_bar['score'] = taipei_bar['rating'] * taipei_bar['user_ratings_total']
+        bar_df = taipei_bar.sort_values('score', ascending=False).head(15)
+        bar_df['營業時間'] = bar_df['opening_hours'].apply(time_transform)
+        bar_df['圖片'] = bar_df['photos'].apply(image_transform)
+        
+        ids = []
+        results = []
+        query_result = gmaps.places_nearby(type='lodging',location=loc, radius=1000)
+        results.extend(query_result['results'])
+        while query_result.get('next_page_token'):
+            time.sleep(2)
+            query_result = gmaps.places_nearby(page_token=query_result['next_page_token'])
+            results.extend(query_result['results'])
+        #print(f'Number of hotels in {query}: {len(results)} (maximum: 60)')
+        for place in results:
+            ids.append(place['place_id'])
+        stores_info = []
+        ids = list(set(ids)) 
+        for id in ids:
+            stores_info.append(gmaps.place(place_id=id, language='zh-TW')['result'])
+        #print(f'Total number of hotels: {len(stores_info)}')
+        taipei_hotel = pd.DataFrame.from_dict(stores_info)
+        taipei_hotel['score'] = taipei_hotel['rating'] * taipei_hotel['user_ratings_total']
+        hotel_df = taipei_hotel.sort_values('score', ascending=False).head(15)
+        hotel_df['營業時間'] = hotel_df['opening_hours'].apply(time_transform)
+        hotel_df['圖片'] = hotel_df['photos'].apply(image_transform)
+    else:
+        stores_info = gmaps.places(query, language='zh-TW', type='bar')['results']
+        #print(f'Total number of bars: {len(stores_info)}')
+        taipei_bar = pd.DataFrame.from_dict(stores_info)
+        taipei_bar['score'] = taipei_bar['rating'] * taipei_bar['user_ratings_total']
+        bar_df = taipei_bar.sort_values('score', ascending=False).head(15)
+        bar_df['營業時間'] = bar_df['opening_hours'].apply(time_transform)
+        bar_df['圖片'] = bar_df['photos'].apply(image_transform)
+        
+        stores_info = gmaps.places(query, language='zh-TW', type='lodging')['results']
+        #print(f'Total number of hotels: {len(stores_info)}')
+        taipei_hotel = pd.DataFrame.from_dict(stores_info)
+        taipei_hotel['score'] = taipei_hotel['rating'] * taipei_hotel['user_ratings_total']
+        hotel_df = taipei_hotel.sort_values('score', ascending=False).head(15)
+        hotel_df['營業時間'] = hotel_df['opening_hours'].apply(time_transform)
+        hotel_df['圖片'] = hotel_df['photos'].apply(image_transform)
+        
+    if place_type == 'A&bar':
+        return bar_df
+    elif place_type == 'A&hotel':
+        return hotel_df
+    return pd.concat([bar_df, hotel_df])
+
+
+# In[63]:
+a = find_sex_place(query = result[1], query_type = 'station', place_type = result[0])
+a.head(5)
+
+#妤文
+#from more options import factorial #看更多選項
+def star_count(rating_num): #依評價顯示星星的函數
+    if rating_num >= 4.5:
+        "contents":[ 
+              {
+                "type": "icon",
+                "size": "xs",
+                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+              }*5,
+              {
+                "type": "text",
+                "text": "5.0",
+                "size": "xs",
+                "color": "#8c8c8c",
+                "margin": "md",
+                "flex": 0
+              }
+            ]
+    elif 3.5 <= rating_num < 4.5:
+        "contents":[ 
+              {
+                "type": "icon",
+                "size": "xs",
+                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+              }*4,
+              {
+                "type": "icon",
+                "size": "xs",
+                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
+              },
+              {
+                "type": "text",
+                "text": "4.0",
+                "size": "xs",
+                "color": "#8c8c8c",
+                "margin": "md",
+                "flex": 0
+              }
+            ]            
+    elif 2.5 <= rating_num < 3.5:
+        "contents":[ 
+              {
+                "type": "icon",
+                "size": "xs",
+                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+              }*3,
+              {
+                "type": "icon",
+                "size": "xs",
+                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
+              }*2,
+              {
+                "type": "text",
+                "text": "3.0",
+                "size": "xs",
+                "color": "#8c8c8c",
+                "margin": "md",
+                "flex": 0
+              }
+            ]
+    elif 1.5 <= rating_num < 2.5:
+        "contents":[ 
+              {
+                "type": "icon",
+                "size": "xs",
+                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+              }*2,
+              {
+                "type": "icon",
+                "size": "xs",
+                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
+              }*3,
+              {
+                "type": "text",
+                "text": "2.0",
+                "size": "xs",
+                "color": "#8c8c8c",
+                "margin": "md",
+                "flex": 0
+              }
+            ]
+    elif rating_num < 1.5:
+        "contents":[ 
+              {
+                "type": "icon",
+                "size": "xs",
+                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+              }*1,
+              {
+                "type": "icon",
+                "size": "xs",
+                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
+              }*4,
+              {
+                "type": "text",
+                "text": "1.0",
+                "size": "xs",
+                "color": "#8c8c8c",
+                "margin": "md",
+                "flex": 0
+              }
+            ]
+		
+def callback():
+# get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+# get request body as text body = request.get_data(as_text=True) app.logger.info("Request body: " + body) # handle webhook body try:     handler.handle(body, signature) except InvalidSignatureError:     abort(400) return 'OK'
+# 訊息傳遞區塊
+# 基本上程式編輯都在這個function
+@handler.add(MessageEvent, message=TextMessage)
+def order_reply(rank): #選擇訂房方式
+    message = text = event.message.text
+    if re.match('我要訂房',message)
+        flex_message = TextSendMessage(text='請選擇訂房方式',
+                            quick_reply=QuickReply(items=[
+                                QuickReplyButton(action=MessageAction(label="電話預訂", text=output.iloc[rank]['formatted_phone_number'])),
+                                QuickReplyButton(action=MessageAction(label="線上預訂", text=output.iloc[rank]['website'])),
+                            ]))
+        line_bot_api.reply_message(event.reply_token, flex_message)
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
+        
+@handler.add(MessageEvent, message=TextMessage)
+def three_handle_message_1(event): #推薦三個旅館/酒吧
+    output = find_sex_place(query=result[1], query_type='station', place_type=result[0]),
+    message = text=event.message.text
+     #if re.match('告訴我秘密',message):
+         # Flex Message Simulator網頁：https://developers.line.biz/console/fx/
+         #flex_message = FlexSendMessage(
+             #alt_text='行銷搬進大程式',
+    contents={
+    "type": "carousel",
+    "contents": [
+    { 
+      "type": "bubble", #旅館01
+      "size": "micro",
+      "hero": {
+        "type": "image", #https://xdasu.com/2018/06/08/python-%e4%bd%bf%e7%94%a8google-map-api6-%e5%8f%96%e5%be%97%e7%85%a7%e7%89%87/
+        "url": output.iloc[0]['圖片'], #主圖01*
+        "size": "full",
+        "aspectMode": "cover",
+        "aspectRatio": "320:213"
+      },
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "text",
+            "text": output.iloc[0]['name']
+            "weight": "bold",
+            "size": "sm",
+            "wrap": True
+          },
+          {
+            "type": "box",
+            "layout": "baseline",
+            "contents": star_num(output.iloc[0]['rating']) #還需要匯入評價*
+          },
+          {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+              {
+                "type": "box",
+                "layout": "baseline",
+                "spacing": "sm",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "地址："+str(output.iloc[0]['formatted_address']), 
+                    "wrap": True,
+                    "color": "#8c8c8c",
+                    "size": "xs",
+                    "flex": 5
+                  }
+                ]
+              },
+              {
+                "type": "text",
+                "text": output.iloc[0]['business_status'], 
+                "wrap": True,
+                "color": "#8c8c8c",
+                "size": "xs",
+                "flex": 5
+              },
+              {
+                "type": "text",
+                "text": "營業時間"+str(output.iloc[0]['營業時間']), #營業時間01*
+                "wrap": True,
+                "color": "#8c8c8c",
+                "size": "xs",
+                "flex": 5
+              }
+              
+            ]
+          }
+        #兩個按鈕
+        "footer": { 
+            "type": "box",
+            "layout": "vertical",
+            "spacing": "sm",
+            "contents": [
+              {
+                "type": "button",
+                "style": "link",
+                "height": "sm",
+                "action": {
+                  "type": "uri",
+                  "label": "看更多",
+                  "uri": "https://linecorp.com" #網址01*
+                }
+              },
+              {
+                "type": "button",
+                "style": "link",
+                "height": "sm",
+                "action": {
+                  "type": "message",
+                  "label": "我要訂房",
+                  "text": "訂房方式" #跳出訂房方式:線上/電話
+                  handle_message(0)
+                  "data":result[0], output.iloc[0]['name'] 
+                }
+              }
+            ],
+            "flex": 0
+          }          
+        ],
+        "spacing": "sm",
+        "paddingAll": "13px"
+      }
+    },
+    #旅館02
+    {
+      "type": "bubble",
+      "size": "micro",
+      "hero": {
+        "type": "image",
+        "url": output.iloc[1]['圖片'], #主圖02
+        "size": "full",
+        "aspectMode": "cover",
+        "aspectRatio": "320:213"
+      },
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "text",
+            "text": output.iloc[1]['name'],
+            "weight": "bold",
+            "size": "sm",
+            "wrap": True
+          },
+          {
+            "type": "box",
+            "layout": "baseline",
+            "contents":star_num(output.iloc[1]['rating'])
+          },
+          {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+              {
+                "type": "box",
+                "layout": "baseline",
+                "spacing": "sm",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "地址："+str(output.iloc[1]['formatted_address']),
+                    "wrap": True,
+                    "color": "#8c8c8c",
+                    "size": "xs",
+                    "flex": 5
+                  }
+                ]
+              },
+              {
+                "type": "text",
+                "text":output.iloc[1]['business_status'],
+                "wrap": True,
+                "color": "#8c8c8c",
+                "size": "xs",
+                "flex": 5
+              },
+              {
+                "type": "text",
+                "text": "營業時間"+str(output.iloc[1]['營業時間']),
+                "wrap": True,
+                "color": "#8c8c8c",
+                "size": "xs",
+                "flex": 5
+              }
+            ]
+          }
+        #兩個按鈕
+         "footer": { 
+            "type": "box",
+            "layout": "vertical",
+            "spacing": "sm",
+            "contents": [
+              {
+                "type": "button",
+                "style": "link",
+                "height": "sm",
+                "action": {
+                  "type": "uri",
+                  "label": "看更多",
+                  "uri": "https://linecorp.com"
+                }
+              },
+              {
+                "type": "button",
+                "style": "link",
+                "height": "sm",
+                "action": {
+                  "type": "message",
+                  "label": "我要訂房",
+                  "text": "我要訂房" #跳出訂房方式:線上/電話
+                  "data":result[0], output.iloc[1]['name'] 
+                  handle_message(1)
+                }
+              }
+            ],
+            "flex": 0
+      }
+        ],
+        "spacing": "sm",
+        "paddingAll": "13px"
+      }
+    },
+    #旅館03
+    {
+      "type": "bubble",
+      "size": "micro",
+      "hero": {
+        "type": "image",
+        "url": output.iloc[2]['圖片'], #主圖03
+        "size": "full",
+        "aspectMode": "cover",
+        "aspectRatio": "320:213"
+      },
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "text",
+            "text": output.iloc[2]['name'],
+            "weight": "bold",
+            "size": "sm"
+            "wrap": True
+          },
+          {
+            "type": "box",
+            "layout": "baseline",
+            "contents": star_num(output.iloc[2]['rating']) #還需要匯入評價*
+          },
+          {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+              {
+                "type": "box",
+                "layout": "baseline",
+                "spacing": "sm",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "地址："+str(output.iloc[2]['formatted_address']),
+                    "wrap": True,
+                    "color": "#8c8c8c",
+                    "size": "xs",
+                    "flex": 5
+                  }
+                ]
+              },
+              {
+                "type": "text",
+                "text": output.iloc[2]['business_status'],
+                "wrap": True,
+                "color": "#8c8c8c",
+                "size": "xs",
+                "flex": 5
+              },
+              {
+                "type": "text",
+                "text": "營業時間"+str(output.iloc[2]['營業時間']),
+                "wrap": True,
+                "color": "#8c8c8c",
+                "size": "xs",
+                "flex": 5
+              }
+            ]
+          }
+        ],
+        "spacing": "sm",
+        "paddingAll": "13px"
+      },
+    #兩個按鈕
+     "footer": {
+        "type": "box",
+        "layout": "vertical",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "button",
+            "style": "link",
+            "height": "sm",
+            "action": {
+              "type": "uri",
+              "label": "看更多",
+              "uri": "https://linecorp.com"
+            }
+          },
+          {
+            "type": "button",
+            "style": "link",
+            "height": "sm",
+            "action": {
+              "type": "message",
+              "label": "我要訂房",
+              "text": "我要訂房" #跳出訂房方式:線上/電話
+              "data":result[0], output.iloc[2]['name'] 
+              handle_message(2)
+              
+            }
+          }
+        ],
+        "flex": 0
+      }
+    }
+    ]
+} #json
+         line_bot_api.reply_message(event.reply_token, flex_message)
+         result = event.postback.data
+'''
 # 効 Review Part
 # input
 
